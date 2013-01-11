7d8545c29772e7ff5a8eb7f4d07195b8ce5c2bfc
jeffschwartz <jefftschwartz@gmail.com>
2013-01-11 15:21:33 -0500

    Final changes prior to publishing article on my blog

--- script/script.js
+++ script/script.js
@@ -6,6 +6,13 @@
  *
  * Code examples covering various use cases of jQuery's $.Deferred and Promise:
  *
+ * 1. Example #1 - calling getData1 to obtain an integer using Promise.done & Promise.fail
+ * 2. Example #2 - calling getData2 to obtain an integer using $.when and Promise.then
+ * 3. Example #3 - calling getData3, getData4 in parallel to obtain 2 integers using $.when and then summing the 2
+ * returned integer values using Promise.done
+ * 4. Example #4 - calling getData5, getData6 in parallel to obtain 2 integers using $.when and then calling getSum
+ * using Promise.then to sum the 2 returned integer values and obtaining the result using Promise.done*
+ *
  * For background information on jQuery $.Deferred and Promise please read the following:
  *
  * 1. jQuery Deferred documentation, at http://api.jquery.com/category/deferred-object/
